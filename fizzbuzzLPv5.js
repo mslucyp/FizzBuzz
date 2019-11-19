8071bdd73fec7ed5de1433c3db787325e1c2cf2b
mslucyp <56561941+mslucyp@users.noreply.github.com>
2019-11-19 20:30:47 +0000

    Update fizzbuzzLPv5.js

--- fizzbuzzLPv5.js
+++ fizzbuzzLPv5.js
@@ -7,7 +7,8 @@ if ( i % 3 == 0 && i % 5 == 0 ){
     output( "FizzBuzz");
 }
 else if  ( i % 3 == 0 ){
-    output( "Fizz");}  
+    output( "Fizz");
+}  
      
 else if ( i % 5 == 0 ){
     output( "Buzz");
